03ee096afb072a011cd44ee9ad26b3a36449ea5b
github username <github email address>
2020-03-13 17:00:52 -0400

    commit

--- app/models/shuttle.rb
+++ app/models/shuttle.rb
@@ -23,4 +23,14 @@ class Shuttle
             puts "Sorry this mission is already full!" 
         end 
     end 
+    def self.find_by_model(model)
+        Shuttle.all.find do |space_ship| 
+            space_ship.model == self 
+        end 
+    end 
+    def current_astronauts
+        missions.map do |astronaut| 
+            mission.astronaut 
+        end 
+    end 
 end
