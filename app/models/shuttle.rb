043010d2dea364b62dddcd83e1de8fbb319a5ede
github username <github email address>
2020-03-13 16:29:55 -0400

    commit

--- app/models/shuttle.rb
+++ app/models/shuttle.rb
@@ -1,7 +1,5 @@
 class Shuttle
-
     attr_reader :model, :capacity 
-
     @@all = []
 
     def initialize(model, capacity)
@@ -9,13 +7,19 @@ class Shuttle
         @capacity = capacity 
         Shuttle.all << self 
     end 
-
     def self.all 
         @@all 
     end 
-
-    def add_astronaut
-        
-    end
-
+    def missions 
+        Mission.all.select do |mission| 
+            mission.shuttle == self 
+        end 
+    end 
+    def add_astronaut(astronaut, launch_date)
+        if self.missions.length < self.capacity 
+            Mission.new(launch_date, astronaut, self) 
+        else 
+            puts "Sorry this mission is already full!" 
+        end 
+    end 
 end
