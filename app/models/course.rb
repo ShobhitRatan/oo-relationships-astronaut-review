49b4804fdf57d70ef6e52e53206e13c6a911a001
github username <github email address>
2020-03-14 17:25:25 -0400

    commit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,13 @@
 class Course 
-    attr_reader :course_code, :dept, :course_num, :course_description 
+    attr_reader :course_code, :dept, :course_num, :course_description, :capacity
     @@all = []
 
-    def initialize(course_code, dept, course_num, course_description) 
+    def initialize(course_code, dept, course_num, course_description, capacity) 
         @course_code = course_code 
         @dept = dept 
         @course_num = course_num 
         @course_description = course_description 
+        @capacity = capacity 
         Course.all << self 
     end 
     def self.all 
@@ -17,6 +18,12 @@ class Course
             enrollment.course == self 
         end 
     end 
+    def add_student(student, semester, grade)
+        if self.enrollments.length < self.enrollments.capacity 
+            Enrollment.new(semester, grade, self, student) 
+        else  
+            puts "This class is full. Please register for another class." 
+        end 
+    end 
     
-
 end 
