b8ceddf75516d5c6852a5535499aa0f02a1de420
github username <github email address>
2020-03-14 16:16:27 -0400

    commit

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,19 +1,15 @@
 class Mission
-
-    attr_accessor :astronaut, :shuttle
-    attr_reader :launch_date
-
+    attr_reader :launch_date 
+    attr_accessor :astronaut, :shuttle 
     @@all = []
 
     def initialize(launch_date, astronaut, shuttle)
-        @launch_date = launch_date
-        @astronaut = astronaut
-        @shuttle = shuttle
-        Mission.all << self
-    end
-
-    def self.all
-        @@all
-    end
-
+        @launch_date = launch_date 
+        @astronaut = astronaut 
+        @shuttle = shuttle 
+        Mission.all << self 
+    end 
+    def self.all 
+        @@all 
+    end 
 end
