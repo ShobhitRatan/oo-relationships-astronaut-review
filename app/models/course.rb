bb5ee7b9faa894cf0767a63cd55ad585c96c0a17
github username <github email address>
2020-03-14 16:58:07 -0400

    commit

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,15 @@
 class Course 
-    attr_reader 
+    attr_reader :course_code, :dept, :course_num, :course_description 
+    @@all = []
+
+    def initialize(course_code, dept, course_num, course_description) 
+        @course_code = course_code 
+        @dept = dept 
+        @course_num = course_num 
+        @course_description = course_description 
+        Course.all << self 
+    end 
+    def self.all 
+        @@all 
+    end 
+end 
