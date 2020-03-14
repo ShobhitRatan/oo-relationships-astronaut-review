5afa93a5d93a4d674a0853be0c957d727448a2a1
github username <github email address>
2020-03-14 16:48:34 -0400

    commit

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,3 +1,15 @@
 class Enrollment 
     attr_reader :semester, :grade 
-    attr_accessor :Student, :
+    attr_accessor :student, :course 
+    @@all = []
+
+    def initialize(semester, grade, course) 
+        @semester = semester 
+        @grade = grade 
+        @course = course 
+        Enrollment.all << self 
+    end 
+    def self.all 
+        @@all 
+    end 
+end 
